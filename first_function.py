db75d88c2be632518c48e317d3a1eb13e490d7a0
k-acheampong <kwameacheampong1@gmail.com>
2021-02-23 17:51:54 -0500

    Change to first function

--- first_function.py
+++ first_function.py
@@ -1,5 +1,7 @@
 def weird_arithmetic(x, y, z):  # function definition line
-    print((x ** x + y ** z) // z)    # code block
+    output = ((x ** x + y ** z) // z)  # code block
+    return output
 
 
-weird_arithmetic(5, 6, 7)  # function call with passed parameters
+some_number = weird_arithmetic(5, 6, 7)  # function call with passed parameters
+print("variable some_number:", some_number)
