9939cb081254da5a2953941f6e3bc51193ad53c0
k-acheampong <kwameacheampong1@gmail.com>
2021-02-23 17:35:23 -0500

    Second commit

--- introduction.py
+++ introduction.py
@@ -4,4 +4,7 @@ num2 = 5
 print("Hello,", name + "!")
 print("Addition:", num1 + num2)
 print("Subtraction:", num1 - num2)
+print("Division:", num1 / num2)
+print("Division, sans decimal places:", num1 // num2)
+print("Exponent:", num1 ** num2)
 print("Modulo:", num1 % num2)
