b9e6e576587cbe181398a8af46df53801860a313
k-acheampong <kwameacheampong1@gmail.com>
2021-02-24 15:51:42 -0500

    practice for tuples and sets

--- dictionary.py
+++ dictionary.py
@@ -2,3 +2,15 @@ dictionaryOfNames = {'blue': "Kwame", 'white': "Bisrat", 'grey': "Jacob"}
 
 name = dictionaryOfNames['blue']
 print(name)
+
+values = dictionaryOfNames.values()
+keys = dictionaryOfNames.keys()
+items = dictionaryOfNames.items()
+
+print(values)
+print(keys)
+print(items)
+
+print(list(values))
+print(list(keys))
+print(list(items))
---
+++ tuples_and _sets.py
@@ -0,0 +1,8 @@
+tupple = (1, 2, 3, 4, 5)
+print(type(tupple))
+
+output = tupple[2]
+print(output)
+
+set_of_unique_numbers = set([1, 1, 2, 2, 3, 3, 4, 4, 5, 6, 7])
+print(set_of_unique_numbers)
