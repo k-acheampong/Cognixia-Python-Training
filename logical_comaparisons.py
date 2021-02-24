c96d3a1ccbc4214640c91421a764bc4c0e3a1995
k-acheampong <kwameacheampong1@gmail.com>
2021-02-24 16:10:32 -0500

    logical comparison

---
+++ logical_comaparisons.py
@@ -0,0 +1,10 @@
+print(1 < 2)
+print(5 < 5)
+print(5 == 5)
+print(5 != 5)
+print(5 <= 5)
+
+print(True and True)  # True
+print(True and False)  # False
+print(True or False)  # True
+print(not True)  # False
