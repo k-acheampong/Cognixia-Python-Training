496a848b6304d7e492fb47517e879377e1247c6f
k-acheampong <kwameacheampong1@gmail.com>
2021-02-24 15:11:42 -0500

    practice for lists

---
+++ basic_data_structures.py
@@ -0,0 +1,12 @@
+listOfNumbers = [1, 2, 3, 4, 5, 6, 7, 8]
+
+# a = listOfNumbers[2:5]
+# b = listOfNumbers[5:7]
+
+# print(a)
+# print(b)
+
+# print(a+b)
+
+listOfNumbers.append(10)
+print(listOfNumbers)
